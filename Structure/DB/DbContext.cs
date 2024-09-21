5b184e0c5d0bf5cfbd672e3e9c55cab33ebbc77c
Robert <robertmaio@outlook.com>
2024-09-21 14:25:58 -0300

    implementando migrations para ter acesso ao sqlserver

--- Structure/DB/DbContext.cs
+++ Structure/DB/DbContext.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Domains.Entities;
 
 namespace minimal_api.Structure.DB
 {
-    public class DbContext
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configuracaoAppSettings;
+        public DbContexto(IConfiguration configuracaoAppSettings)
+        {
+            _configuracaoAppSettings = configuracaoAppSettings;
+        }
+        public DbSet<Administrator> Admins {get; set;} = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured){
+                var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+                if(!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(stringConexao,
+                    ServerVersion.AutoDetect(stringConexao)
+                    );
+                }
+            }
+            
+        }
     }
 }
